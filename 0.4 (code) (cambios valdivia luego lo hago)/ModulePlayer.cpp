d3e251da458a57627317578ee47d39a5ff9dec40
AwesomeSuitGuy <davidvm12@tr12.cat>
2017-04-06 16:23:09 +0200

    0.4

---
+++ 0.4 (code) (cambios valdivia luego lo hago)/ModulePlayer.cpp
@@ -0,0 +1,149 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleTextures.h"
+#include "ModuleInput.h"
+#include "ModuleRender.h"
+#include "ModulePlayer.h"
+#include "ModuleCollision.h"
+#include "Animation.h"
+#include "ModuleParticles.h"
+
+ModulePlayer::ModulePlayer()
+{
+	position.x = 50;
+	position.y = 2610;
+
+	// idle animation
+	idle.PushBack({ 9, 6, 27, 48 }); //1
+	idle.PushBack({ 107, 7, 27, 48 }); //2
+	idle.PushBack({ 61, 96, 27, 48 }); //3
+	idle.PushBack({ 112, 97, 27, 48 }); //4
+	idle.PushBack({ 159, 5, 27, 48 }); //5
+	idle.PushBack({ 53, 6, 27, 48 }); //6
+	idle.PushBack({ 14, 95, 27, 48 }); //7
+	idle.PushBack({ 163, 99, 27, 48 }); //8
+	idle.speed = 0.08f;
+
+	// left animation
+	left.PushBack({ 18, 417, 24, 47 });
+	left.PushBack({ 73, 418, 24, 47 });
+	left.PushBack({ 122, 419, 24, 47 });
+	left.PushBack({ 170, 420, 24, 46 });
+	left.loop = false;
+	left.speed = 0.08f;
+
+	// more left animation
+	mleft.PushBack({ 18, 350, 26, 48 });
+	mleft.PushBack({ 73, 351, 27, 48 });
+	mleft.PushBack({ 120, 351, 27, 48 });
+	mleft.PushBack({ 170, 351, 27, 48 });
+	mleft.loop = true;
+	mleft.speed = 0.08f;
+
+	// right animation
+	right.PushBack({ 70, 171, 26, 48 });
+	right.PushBack({ 119, 171, 27, 48 });
+	right.PushBack({ 168, 172, 27, 48 });
+	right.PushBack({ 15, 174, 27, 48 });
+	right.loop = false;
+	right.speed = 0.08f;
+
+	// more right animation
+	mright.PushBack({ 20, 246, 27, 47 });
+	mright.PushBack({ 79, 248, 27, 47 });
+	mright.PushBack({ 121, 247, 27, 46 });
+	mright.PushBack({ 171, 248, 27, 46 });
+	mright.loop = true;
+	mright.speed = 0.08f;
+
+	// Shoot animation
+	shoot.PushBack({ 34,1516,80,130 });
+	shoot.PushBack({ 129,1516,90,130 });
+	shoot.PushBack({ 245,1531,90,130 });
+	shoot.PushBack({ 351,1531,400,130 });
+	shoot.speed = 0.005f;
+}
+
+ModulePlayer::~ModulePlayer()
+{}
+
+// Load assets
+bool ModulePlayer::Start()
+{
+	LOG("Loading player textures");
+	bool ret = true;
+	graphics = App->textures->Load("assets/Yuan_Nang.png");
+	collider = App->collision->AddCollider({ position.x, position.y, 27, -48 }, COLLIDER_PLAYER, this);
+	return ret;
+}
+
+bool ModulePlayer::CleanUp()
+{
+	LOG("Unloading player 1");
+	App->textures->Unload(graphics);
+	App->collision->EraseCollider(collider);
+
+	return true;
+}
+
+// Update: draw background
+update_status ModulePlayer::Update()
+{
+	Animation* current_animation = &idle;
+
+	int speed = 3;
+
+	if (App->input->keyboard[SDL_SCANCODE_RIGHT] == KEY_STATE::KEY_REPEAT)
+	{
+		current_animation = &right;
+		current_animation = &mright;
+		position.x += speed;
+		if (position.x > (SCREEN_WIDTH - 27)) {
+			position.x = (SCREEN_WIDTH - 27);
+		}
+	}
+	if (App->input->keyboard[SDL_SCANCODE_LEFT] == KEY_STATE::KEY_REPEAT)
+	{
+		current_animation = &left;
+		current_animation = &mleft;
+		position.x -= speed;
+		if (position.x < 0) {
+			position.x = 0;
+		}
+	}
+	if (App->input->keyboard[SDL_SCANCODE_UP] == KEY_STATE::KEY_REPEAT)
+	{
+		current_animation = &idle;
+		position.y -= speed;
+		if (position.y < 2376) {
+			position.y = 2376;
+		}
+	}
+	if (App->input->keyboard[SDL_SCANCODE_DOWN] == KEY_STATE::KEY_REPEAT)
+	{
+		current_animation = &idle;
+		position.y += speed;
+		if (position.y >(SCREEN_HEIGHT + 2330)) {
+			position.y = (SCREEN_HEIGHT + 2330);
+		}
+	}
+
+	if (App->input->keyboard[SDL_SCANCODE_X] == KEY_STATE::KEY_DOWN)
+	{
+		App->particles->AddParticle(App->particles->laser, position.x + 9, position.y - 30, COLLIDER_PLAYER_SHOT);
+
+	}
+
+	if (App->input->keyboard[SDL_SCANCODE_LEFT] == KEY_STATE::KEY_REPEAT
+		&& App->input->keyboard[SDL_SCANCODE_RIGHT] == KEY_STATE::KEY_REPEAT)
+		current_animation = &idle;
+
+
+	collider->SetPos(position.x, position.y);
+	// Draw everything --------------------------------------
+	SDL_Rect r = current_animation->GetCurrentFrame();
+
+	App->render->Blit(graphics, position.x, position.y - r.h, &r);
+
+	return UPDATE_CONTINUE;
+}
---
+++ 0.4 (code) (cambios valdivia luego lo hago)/ModuleScoreScreen.cpp
@@ -0,0 +1,74 @@
+#include "Globals.h"
+#include "Application.h"
+#include "ModuleTextures.h"
+#include "ModuleRender.h"
+#include "ModuleInput.h"
+#include "ModuleTitleScreen.h"
+#include "ModuleScoreScreen.h"
+#include "ModulePlayer.h"
+#include "ModuleFadeToBlack.h"
+
+
+ModuleScoreScreen::ModuleScoreScreen()
+{
+	scorescreen.x = 0;
+	scorescreen.y = 0;
+	scorescreen.w = 224;
+	scorescreen.h = 320;
+}
+
+ModuleScoreScreen::~ModuleScoreScreen()
+{}
+
+// Load assets
+bool ModuleScoreScreen::Start()
+{
+	App->render->camera.x = 0;
+	App->render->camera.y = 0;
+
+	LOG("Loading score screen assets");
+	bool ret = true;
+	graphics = App->textures->Load("assets/Score.png");
+
+	LOG("Init SDL audio");
+	if (SDL_Init(SDL_INIT_AUDIO) < 0)
+	{
+		LOG("SDL_AUDIO could not initialize! SDL_Error:\n");
+		LOG(SDL_GetError());
+		ret = false;
+	}
+	else
+	{
+		Mix_Init(MIX_INIT_OGG);
+		Mix_OpenAudio(24000, MIX_DEFAULT_FORMAT, 2, 2048);
+		music = Mix_LoadMUS("assets/gunbird-014_Scores.ogg");
+		Mix_PlayMusic(music, -1);
+	}
+
+	return ret;
+}
+
+bool ModuleScoreScreen::CleanUp()
+{
+	LOG("Destroying SDL audio");
+	Mix_FreeMusic(music);
+
+	LOG("Unloading score screen");
+	App->textures->Unload(graphics);
+
+	return true;
+}
+
+// Update: draw background
+update_status ModuleScoreScreen::Update()
+{
+	// Draw everything --------------------------------------
+
+	App->render->Blit(graphics, 0, 0, &scorescreen, 0.75f);
+
+	if (App->input->keyboard[SDL_SCANCODE_SPACE] == 1) {
+		App->fade->FadeToBlack(this, App->titlescreen, 1);
+	}
+
+	return UPDATE_CONTINUE;
+}
