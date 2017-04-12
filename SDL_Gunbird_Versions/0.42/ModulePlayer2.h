de95fb1ddcf3889169d6fccf508ddd07fd424236
AwesomeSuitGuy <d.valdiviamartinez@gmail.com>
2017-04-12 17:07:42 +0200

    0.42 (con el castillo)

---
+++ SDL_Gunbird_Versions/0.42/ModulePlayer2.h
@@ -0,0 +1,35 @@
+#ifndef __ModulePlayer2_H__
+#define __ModulePlayer2_H__
+
+#include "Module.h"
+#include "Animation.h"
+#include "Globals.h"
+#include "p2Point.h"
+
+struct SDL_Texture;
+struct Collider;
+
+class ModulePlayer2 : public Module
+{
+public:
+	ModulePlayer2();
+	~ModulePlayer2();
+
+	bool Start();
+	update_status Update();
+	bool CleanUp();
+
+	void OnCollision(Collider* c1, Collider* c2);
+
+public:
+
+	SDL_Texture* graphics = nullptr;
+	Collider* collider = nullptr;
+	Animation idle;
+	Animation right;
+	Animation left;
+	iPoint position;
+
+};
+
+#endif
