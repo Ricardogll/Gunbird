fbb7c23469a42d5fc644e724e8840e20f3766627
AwesomeSuitGuy <d.valdiviamartinez@gmail.com>
2017-06-04 04:47:45 +0200

    laser kyoko

---
+++ SDL_Gunbird_Versions/0.9 (laser kyoko)/Enemy_turret2.h
@@ -0,0 +1,56 @@
+#ifndef __ENEMY_TURRET2_H__
+#define __ENEMY_TURRET2_H__
+
+#include "Enemy.h"
+
+class Enemy_Turret2 : public Enemy
+{
+private:
+	Animation front;
+	Animation l1;
+	Animation l2;
+	Animation l3;
+	Animation l4;
+	Animation l5;
+	Animation l6;
+	Animation l7;
+	Animation l8;
+	Animation l9;
+	Animation l10;
+	Animation l11;
+	Animation l12;
+	Animation l13;
+	Animation l14;
+
+	Animation back;
+
+	Animation r1;
+	Animation r2;
+	Animation r3;
+	Animation r4;
+	Animation r5;
+	Animation r6;
+	Animation r7;
+	Animation r8;
+	Animation r9;
+	Animation r10;
+	Animation r11;
+	Animation r12;
+	Animation r13;
+	Animation r14;
+	Animation r15;
+
+
+	Animation turret2;
+	Animation turret2_white;
+	iPoint original_pos;
+	bool right;
+
+public:
+	Enemy_Turret2(int x, int y);
+	void Move();
+	void OnCollision(Collider* collider);
+	uint getHitPoints();
+};
+
+#endif
---
+++ SDL_Gunbird_Versions/0.9 (laser kyoko)/ModuleParticles.h
@@ -0,0 +1,119 @@
+#ifndef __MODULEPARTICLES_H__
+#define __MODULEPARTICLES_H__
+
+#include "Module.h"
+#include "Animation.h"
+#include "Globals.h"
+#include "p2Point.h"
+#include "ModuleRender.h"
+#include "ModuleCollision.h"
+
+#define MAX_ACTIVE_PARTICLES 200
+#define SPEED_LASER_PLAYER -10
+#define SPEED_SWORD_YUAN_NANG -5
+
+struct SDL_Texture;
+struct Collider;
+enum COLLIDER_TYPE;
+
+struct Particle
+{
+	Collider* collider = nullptr;
+	Animation anim;
+	uint fx = 0;
+	iPoint position;
+	iPoint speed;
+	Uint32 born = 0;
+	Uint32 life = 0;
+	uint type;
+	bool fx_played = false;
+
+	Particle();
+	Particle(const Particle& p);
+	~Particle();
+	bool Update();
+};
+
+class ModuleParticles : public Module
+{
+public:
+	ModuleParticles();
+	~ModuleParticles();
+
+	bool Start();
+	update_status Update();
+	bool CleanUp();
+	void OnCollision(Collider* c1, Collider* c2);
+
+	void AddParticle(const Particle& particle, int x, int y, int speed_x, int speed_y, uint type, COLLIDER_TYPE collider_type = COLLIDER_NONE, Uint32 delay = 0);
+
+private:
+
+	SDL_Texture* graphics = nullptr;
+	SDL_Texture* graphics2 = nullptr;
+	SDL_Texture* graphics3 = nullptr;
+	//uint last_particle = 0;
+
+	bool up;
+	bool left;
+
+public:
+	Particle* active[MAX_ACTIVE_PARTICLES];
+
+	Particle laser_nang_0;
+	Particle laser_nang_1;
+	Particle laser_nang_2;
+	Particle laser_nang_3;
+	Particle laser_nang_4;
+	Particle laser_nang_5;
+	Particle laser_nang_6;
+	Particle laser_nang_7;
+	Particle laser_nang_8;
+	Particle laser_nang_9;
+	Particle laser_nang_10;
+	Particle laser_nang_11;
+	Particle laser_nang_12;
+	Particle laser_nang_13;
+	Particle laser_nang_14;
+
+	Particle nang_sword;	
+
+	Particle laser_marion_0;
+	Particle laser_marion_1;
+	Particle laser_marion_2;
+
+	Particle laser_marion_left_0;
+	Particle laser_marion_left_1;
+	Particle laser_marion_left_2;
+
+	Particle laser_marion_right_0;
+	Particle laser_marion_right_1;
+	Particle laser_marion_right_2;
+
+	Particle laser_ash;
+
+	Particle laser_tetsu;
+
+	Particle laser_kyoko;
+
+	Particle laser_valnus_0;
+	Particle laser_valnus_1;
+	Particle laser_valnus_2;
+	Particle laser_valnus_3;
+
+	Particle shot_enemy;
+	Particle shot_turret;
+	Particle explosion_turret;
+	Particle explosion_drone;
+	Particle explosion_balloon;
+	Particle power_up;
+
+	uint laser_nang_fx;
+	uint laser_marion_fx;
+	uint laser_ash_fx;
+	uint laser_tetsu_fx;
+	uint laser_valnus_fx;
+
+};
+
+#endif // __MODULEPARTICLES_H__
